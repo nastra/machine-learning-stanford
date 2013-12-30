e982cfbd3eb2c6788d06b9c86651b1a37f4fc37f
nastra <etudenhoefner@gmail.com>
2013-12-30 14:25:01 +0100

    finished all assignments

--- mlclass-ex7/kMeansInitCentroids.m
+++ mlclass-ex7/kMeansInitCentroids.m
@@ -11,16 +11,14 @@ centroids = zeros(K, size(X, 2));
 % ====================== YOUR CODE HERE ======================
 % Instructions: You should set centroids to randomly chosen examples from
 %               the dataset X
-%
 
 
+% Initialize the centroids to be random examples
+% Randomly reorder the indices of examples
+randidx = randperm(size(X, 1));
+% Take the first K examples as centroids
+centroids = X(randidx(1:K), :);
 
-
-
-
-
-
-% =============================================================
 
 end
 
